8f1d04f115591b45894e00329f9cd6bf70dcbc3c
mohamed <91436391+mohamed-benoughidene@users.noreply.github.com>
2023-12-28 00:04:05 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ function App() {
   const channels = ['freecodecamp', 'ESL_SC2', 'OgamingSC2', 'cretetion', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas'];
 
   useEffect(() => {
-  async function fetchData() {
-    const promises = channels.map(channel =>
-      axios.get(`https://twitch-proxy.freecodecamp.rocks/twitch-api/streams/${channel}`)
-    );
-    const responses = await Promise.all(promises);
-    const data = responses.map(response => response.data);
-    setStreamData(data);
-  }
-  fetchData();
-}, [channels]);
+    async function fetchData() {
+      const promises = channels.map(channel =>
+        axios.get(`https://twitch-proxy.freecodecamp.rocks/twitch-api/streams/${channel}`)
+      );
+      const responses = await Promise.all(promises);
+      const data = responses.map(response => response.data);
+      setStreamData(data);
+    }
+    fetchData();
+  }, []);
 console.log(streamData);
   return (
     <div className='container'>
